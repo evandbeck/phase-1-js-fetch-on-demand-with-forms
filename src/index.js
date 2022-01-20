e8995eb7d4effbab09cfad726f0fca934427b5fc
Evan Beck <evandrydenbeck@gmail.com>
2022-01-19 22:26:11 -0500

    commit tree 0b2b11bcb7706790746fe8fdd0a8ce6000021be1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', event => {
+        event.preventDefault();
+        // console.log(event.target.searchByID.value);
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(movies => {
+            const titles = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            
+            titles.innerText = movies.title;
+            summary.innerText = movies.summary;
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
